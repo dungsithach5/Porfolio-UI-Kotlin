2ec0ec9d99daf2bc5b8c005aa36c65e94bfa3ac2
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2024-05-16 12:43:38 +0700

    Update UI

--- app/src/main/java/com/example/portfolioui/MainActivity.kt
+++ app/src/main/java/com/example/portfolioui/MainActivity.kt
@@ -4,13 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.portfolioui.ui.theme.PortfolioUITheme
 
 class MainActivity : ComponentActivity() {
@@ -19,14 +52,109 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PortfolioUITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val status = remember {
+                    mutableStateOf(false)
+                }
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(390.dp)
+                            .padding(12.dp),
+                        shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier.height(300.dp),
+                            verticalArrangement = Arrangement.Top,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            CreateImageProfile()
+                            CreateInfo()
+                            Button(onClick = {
+                                status.value = !status.value
+                            }) {
+                                Text(text = "Portfolio", style = MaterialTheme.typography.bodyMedium)
+                            }
+                            Divider()
+                            if (status.value)
+                            CreatePortfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+                        }
+                    }
                 }
             }
         }
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+fun CreateInfo() {
+    Column(modifier = Modifier.padding(3.dp)) {
+        Text(text = "Huynh",
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary)
+
+        Text(text = "Android programmer",
+            modifier = Modifier.padding(3.dp))
+
+        Text(text = "@themeCompose",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(3.dp))
+    }
+}
+
+@Composable
+fun CreatePortfolio(data: List<String>) {
+    LazyColumn {
+        items(data) {
+            item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxSize(),
+                shape = RectangleShape,
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            ) {
+                Row (
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colorScheme.surface)
+                ){
+                    CreateImageProfile()
+                    Column(modifier = Modifier
+                        .padding(7.dp)
+                        .align(alignment = Alignment.CenterVertically)) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great project", style = MaterialTheme.typography.bodyMedium)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CreateImageProfile() {
+    Surface(
+        modifier = Modifier
+            .padding(5.dp)
+            .size(150.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_android_black_24dp),
+            contentDescription = "profile image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop)
     }
 }
 
